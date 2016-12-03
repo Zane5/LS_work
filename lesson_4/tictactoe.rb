2d0916054e3660dd7f4ebf299cf57750f791ed1a
Zane5 <Zane5@user.noreply.github.com>
2016-12-03 04:06:50 +0000

    add joinor method

--- lesson_4/tictactoe.rb
+++ lesson_4/tictactoe.rb
@@ -79,6 +79,17 @@ def detect_winner(brd)
   nil
 end
 
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
 loop do
   board = intalize_board
 
